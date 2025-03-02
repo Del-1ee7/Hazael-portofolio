2753cf0a46ddab382d12bbef43f91bfbf330cf4f
Del-1ee7 <hazaelhagi1337@gmail.com>
2025-03-02 17:06:09 +0700

    message

---
+++ assets/js/Biodata.js
@@ -0,0 +1,31 @@
+const biodata = {
+    avatar: "./asset/images/avatar-4.png",
+    name: "Hazael Hegira",
+    job: "Student",
+    contactList: [
+        {
+            icon: `<ion-icon name="mail-outline"></ion-icon>`,
+            title: "Email",
+            info: "hazaelhagi1338@gmail.com",
+            link: "mailto:hazaelhagi1338@gmail.com"
+        },
+        {
+            icon: `<ion-icon name="call-outline"></ion-icon>`,
+            title: "Phone",
+            info: "+6259-2075-8799",
+            link: "https://wa/me/+62 859-2075-8799"
+        },
+        {
+            icon: `<ion-icon name="calendar-outline"></ion-icon>`,
+            title: "Birthday",
+            info:"May 23, 2007",
+            link:"#"
+        },
+        {
+            icon: `<ion-icon name="location-outline"></ion-icon>`,
+            title: "Location",
+            info: "Tangerang Kota",
+            link:"#"
+        },
+    ]
+}
